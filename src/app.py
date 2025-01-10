20c1d8f7f84f944dc9dd9b3d156e4af631d95802
Fredis Mappin <100321504+FrederickMappin@users.noreply.github.com>
2025-01-10 16:05:59 -0500

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -2,11 +2,15 @@ import streamlit as st
 import pandas as pd
 from utils.smile_image import smiles_to_image
 
+# Set page configuration
+st.set_page_config(layout="wide")
+
+
 def main():
     st.title("Streamlit Dashboard")
 
-    # Upload CSV file
-    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+    # Upload CSV file in the sidebar
+    uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
     
     if uploaded_file is not None:
         # Read the CSV file
@@ -14,19 +18,29 @@ def main():
         
         # Check if 'molecule_chembl_id' column exists
         if 'molecule_chembl_id' in df.columns:
-            # Dropdown menu for selecting molecule
-            selected_molecule = st.selectbox("Select a molecule", df['molecule_chembl_id'])
-            
-            # Get the SMILES string for the selected molecule
-            smiles = df[df['molecule_chembl_id'] == selected_molecule]['smiles'].values[0]
-            
-            # Call the function to get the image
-            try:
-                img = smiles_to_image(smiles)
-                # Display the image
-                st.image(img, caption=f"Chemical Structure of {selected_molecule}")
-            except ValueError as e:
-                st.error(f"Error: {e}")
+            # Create a list with 'all' and 'none' options
+            molecule_options = ['all', 'none'] + df['molecule_chembl_id'].unique().tolist()
+
+            # Dropdown menu for selecting molecule in the sidebar
+            selected_molecule = st.sidebar.selectbox("Select a molecule", molecule_options)
+
+            if selected_molecule == 'all':
+                st.write("All molecules selected.")
+                # Add logic to handle 'all' option if needed
+            elif selected_molecule == 'none':
+                st.write("No molecule selected.")
+                # Add logic to handle 'none' option if needed
+            else:
+                # Get the SMILES string for the selected molecule
+                smiles = df[df['molecule_chembl_id'] == selected_molecule]['smiles'].values[0]
+                
+                # Call the function to get the image
+                try:
+                    img = smiles_to_image(smiles)
+                    # Display the image
+                    st.image(img, caption=f"Chemical Structure of {selected_molecule}")
+                except ValueError as e:
+                    st.error(f"Error: {e}")
         else:
             st.error("The uploaded CSV file does not contain 'molecule_chembl_id' column.")
     else:
